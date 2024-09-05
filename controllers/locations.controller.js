3b7f3b5670d76d31cb97a5deafdef0e91552c918
kingsman44 <shivan.0972@gmail.com>
2024-09-05 14:37:07 +0530

    More chnages

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -1,6 +1,7 @@
 import locations from "../models/locations.model.js"
 import { errorHandler } from '../utils/error.js';
 import trackRecord from "../models/trackrecord.model.js";
+import User from '../models/user.model.js';
 
 export const addLocations = async (req, res, next) => {
   const { name, longitude, latitude } = req.body;
@@ -32,25 +33,28 @@ export const getLocations = async (req, res, next) => {
     let locs = [];
 
     var startDate = new Date();
-
     startDate.setSeconds(0);
     startDate.setHours(0);
     startDate.setMinutes(0);
 
     var dateMidnight = new Date(startDate);
-
     dateMidnight.setHours(23);
     dateMidnight.setMinutes(59);
     dateMidnight.setSeconds(59);
 
-
     for (let i = 0; i < locationsList.length; i++) {
       const locationId = locationsList[i]._id;
-      const uniqueUsers = await trackRecord.distinct('user', {
-        location: locationId, time: {
+
+      const uniqueUserIds = await trackRecord.distinct('user', {
+        location: locationId,
+        time: {
           $gt: startDate,
           $lt: dateMidnight
         }
+      });
+
+      const uniqueUsers = await User.find({
+        _id: { $in: uniqueUserIds }
       });
 
       locs.push({
@@ -58,8 +62,10 @@ export const getLocations = async (req, res, next) => {
         users: uniqueUsers
       });
     }
+
     res.status(200).json(locs);
   } catch (error) {
+    console.log(error);
     next(errorHandler(500, 'Internal Server Error'));
   }
 };
